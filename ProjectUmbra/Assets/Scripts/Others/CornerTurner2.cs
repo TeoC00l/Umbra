096c5d4d9e6924847192841ecc50727996243c59
disaviola <37692445+disaviola@users.noreply.github.com>
2019-10-21 14:38:05 +0200

    fixed bridge and project organizing

---
+++ ProjectUmbra/Assets/Scripts/Others/CornerTurner2.cs
@@ -0,0 +1,43 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class CornerTurner2 : MonoBehaviour
+//{
+//    [SerializeField] private GameObject player;
+//    private Transform rotation0;
+//    private Transform rotation1;
+//    private int currentRotation;
+//    private PlayerMovement pm;
+
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        pm = player.GetComponent<PlayerMovement>();
+//        currentRotation = 0;
+//    }
+
+//    private void OnTriggerEnter(Collider other)
+//    {
+
+//        if (other.CompareTag("Player"))
+//        {
+//            pm.SetControlled(false);
+//            pm.rb.isKinematic = true;
+
+//            if (currentRotation == 0)
+//            {
+//                other.transform.localRotation = Quaternion.Euler(0f, -90f, 0f);
+
+//                currentRotation = 1;
+
+//            }
+//            else
+//            {
+//                other.transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+
+//                currentRotation = 0;
+//            }
+//        }
+//    }
+//}
