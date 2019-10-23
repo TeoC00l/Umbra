c4f916fb3b98f68e91691bb9d1ecee86961bb234
disaviola <37692445+disaviola@users.noreply.github.com>
2019-10-23 14:52:46 +0200

    Main menu & pause menu

---
+++ ProjectUmbra/Assets/Scripts/Managers and controllers/MainMenuManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenuManager : MonoBehaviour
+{
+
+    [SerializeField] private int newGameSceneNumber;
+    [SerializeField] private GameObject mainButtons;
+    [SerializeField] private GameObject validateQuit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(newGameSceneNumber);
+    }
+
+
+    public void Options()
+    {
+
+    }
+
+    public void ApplyAndReturn()
+    {
+
+    }
+
+    public void ResetToDefault()
+    {
+
+    }
+
+    public void ValidateQuit()
+    {
+        if (mainButtons.activeSelf) {
+            mainButtons.SetActive(false);
+        }
+        else if (!mainButtons.activeSelf)
+        {
+            mainButtons.SetActive(true);
+        }
+        if (!validateQuit.activeSelf) { 
+            validateQuit.SetActive(true);
+        }
+        else if (validateQuit.activeSelf)
+        {
+            validateQuit.SetActive(false);
+        }
+
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Hejdå!");
+        Application.Quit();
+    }
+}
---
+++ ProjectUmbra/Assets/Scripts/Managers and controllers/PauseMenuManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuManager : MonoBehaviour
+{
+    [SerializeField] private Canvas pauseMenu;
+    bool isPaused = false;
+    float original;
+
+    public void Start()
+    {
+        original = Time.timeScale;
+    }
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseMenu();
+        }
+    }
+
+    public void PauseMenu()
+    {
+        if (!pauseMenu.gameObject.activeSelf) { 
+            pauseMenu.gameObject.SetActive(true);
+            isPaused = true;
+        } else
+        {
+            pauseMenu.gameObject.SetActive(false);
+            isPaused = false;
+        }
+        if (isPaused)
+        {
+            Time.timeScale = 0;
+        }
+        if (!isPaused)
+        {
+            Time.timeScale = original;
+        }
+    }
+
+    public void Exit()
+    {
+        //SceneManager.blabla byt scen
+        SceneManager.LoadScene(0);
+    }
+}
