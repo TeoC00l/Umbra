6d85e0d0e52847eb3b6d603adc4dcce8a1735de8
Bazzan <sebake01@gmail.com>
2019-10-28 11:24:08 +0100

    WhiteBox

---
+++ ProjectUmbra/Assets/Scripts/Interactive objects/PressurePadTrigger.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePadTrigger : MonoBehaviour
+{
+    private Animator anim;
+    private bool isPressed = false;
+    [SerializeField] GameObject animGO;
+    [SerializeField] string animationBoolKey = "";
+
+
+    void Start()
+    {
+        anim = animGO.GetComponent<Animator>();
+    }
+
+    //Byt ut "ObjectToAnimate" mot objektet som ska animeras, samt animator controllers och animationer i dem.
+    //Skriv villkorsboolens namn i animationBoolKey.
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+
+            if (!isPressed)
+            {
+                anim.SetBool(animationBoolKey, true);
+
+                isPressed = true;
+
+            }
+
+
+
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+
+        if (other.CompareTag("Player"))
+        {
+            if (isPressed)
+            {
+                anim.SetBool(animationBoolKey, false);
+                isPressed = false;
+
+            }
+        }
+        
+    }
+
+}
