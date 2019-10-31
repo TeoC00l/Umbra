7f89cd0f883d8a679feb2aef79686088ea1c9683
disaviola <37692445+disaviola@users.noreply.github.com>
2019-10-31 11:42:26 +0100

    camera fixes & dialogue added

---
+++ ProjectUmbra/Assets/Scripts/Managers and controllers/PlayDialogue.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayDialogue : MonoBehaviour
+{
+    private DialogueTrigger dt;
+
+    private void Start()
+    {
+        dt = GetComponent<DialogueTrigger>();
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            dt.TriggerDialogue();
+            gameObject.SetActive(false);
+        }
+    }
+}
