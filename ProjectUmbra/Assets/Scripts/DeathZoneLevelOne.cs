32ba17928c7991de29d87af0d6fc4cd1a252b57c
disaviola <37692445+disaviola@users.noreply.github.com>
2019-10-31 10:53:33 +0100

    First Cutscene

---
+++ ProjectUmbra/Assets/Scripts/DeathZoneLevelOne.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZoneLevelOne : MonoBehaviour
+{
+
+   [SerializeField] private DeathComponent deathComponent;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        deathComponent.RespawnPlayer();
+
+        MonsterLevelOneScrpit.RespawnMonster();
+
+    }
+}
---
+++ ProjectUmbra/Assets/Scripts/PressurePadMultipleBoolsChild.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePadMultipleBoolsChild : MonoBehaviour
+{
+    public bool isPressed = false;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player") || other.CompareTag("Alice") || other.CompareTag("Set") || other.CompareTag("Grabbable"))
+        {
+            isPressed = true;
+        }
+
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player") || other.CompareTag("Alice") || other.CompareTag("Set"))
+        {
+            isPressed = false;
+        }
+    }
+}
