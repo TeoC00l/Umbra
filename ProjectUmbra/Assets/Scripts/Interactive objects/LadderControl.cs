6c6990274ad51028a45406831f08c61f811bff54
disaviola <37692445+disaviola@users.noreply.github.com>
2019-11-05 09:38:02 +0100

    ladder

--- ProjectUmbra/Assets/Scripts/Interactive objects/LadderControl.cs
+++ ProjectUmbra/Assets/Scripts/Interactive objects/LadderControl.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class LadderControl : MonoBehaviour
 {
     private PlayerMovement pm;
+    private BoxCollider boxCollider;
+
+    public void Start()
+    {
+        boxCollider = GetComponent<BoxCollider>();
+    }
 
     public void OnTriggerEnter(Collider other)
     {
@@ -14,9 +20,13 @@ public class LadderControl : MonoBehaviour
     }
     public void OnTriggerStay(Collider other)
     {
-        if (other.CompareTag("Player") /*&& Input.GetKeyDown(KeyCode.W)*/ && !pm.getLadderStatus())
+        if (other.CompareTag("Player") && (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S)) && !pm.getLadderStatus())
         {
             pm.setLadderStatus(true);
+            Vector3 pos = new Vector3(boxCollider.transform.position.x, other.transform.position.y, other.transform.position.z);
+            //pos = pos.normalized;
+            other.transform.position = pos;
+            //other.transform.Translate(pos);
         }
     }
 
