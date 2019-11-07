a4509c46540dc49ac81c3478d6f918f97757e1db
disaviola <37692445+disaviola@users.noreply.github.com>
2019-11-07 13:34:28 +0100

    ladder fix

---
+++ ProjectUmbra/Assets/Scripts/Interactive objects/MovingPlatform.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    private BoxCollider platformCollider;
+
+
+    private void Start()
+    {
+        platformCollider = GetComponent<BoxCollider>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.gameObject.transform.SetParent(transform.parent);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.gameObject.transform.SetParent(null);
+        }
+    }
+}
