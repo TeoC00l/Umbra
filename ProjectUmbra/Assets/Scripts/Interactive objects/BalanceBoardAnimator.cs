16c046d806ca203c9e1f6c032e434eb910d6e866
disaviola <37692445+disaviola@users.noreply.github.com>
2019-11-07 14:44:50 +0100

    Balance board w/ animation

---
+++ ProjectUmbra/Assets/Scripts/Interactive objects/BalanceBoardAnimator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalanceBoardAnimator : MonoBehaviour
+{
+    [SerializeField] public Animator animator;
+    [SerializeField] private bool isPointA;
+    private int collidersInTrigger = 0;
+
+    private void Start()
+    {
+        animator = GetComponentInParent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(isPointA);
+        collidersInTrigger++;
+        //if(collidersInTrigger >= 2)
+        //{
+        //    animator.SetBool("IsWeighed", isPointA);
+        //}
+        animator.SetBool("IsWeighed", isPointA);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        collidersInTrigger--;
+    }
+}
