2322b2de2be5a6d5a1dd517a340d68da82d22302
Bazzan <sebake01@gmail.com>
2019-11-11 14:48:20 +0100

    Wb1 spawn alice and set, bridge,

---
+++ ProjectUmbra/Assets/ObjectHandeler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectHandeler : MonoBehaviour
+{
+
+    public static GameObject Set;
+    public static GameObject Alice;
+
+
+    private void Awake()
+    {
+        Set = GameObject.FindGameObjectWithTag("Set");
+        Alice = GameObject.FindGameObjectWithTag("Alice");
+
+    }
+
+
+    private void Start()
+    {
+        if(Set != null && Alice != null){
+            Set.SetActive(false);
+            Alice.SetActive(false);
+        }
+    }
+
+
+}
