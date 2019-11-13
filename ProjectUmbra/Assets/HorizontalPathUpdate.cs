072876f267fff33c2ba95591b285ac7d7a45aec7
TeoC00l <37851342+TeoC00l@users.noreply.github.com>
2019-11-13 19:21:51 +0100

    fix

--- ProjectUmbra/Assets/HorizontalPathUpdate.cs
+++ ProjectUmbra/Assets/HorizontalPathUpdate.cs
@@ -1,59 +1,59 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class HorizontalPathUpdate : MonoBehaviour
-{
-    Vector3 position;
-    [SerializeField] private Transform startTransform;
-    private bool isUpdatingXPosition;
-    private bool isCornerTurnerVirgin;
-    CheckPointManager checkPointManager;
+//public class HorizontalPathUpdate : MonoBehaviour
+//{
+//    Vector3 position;
+//    [SerializeField] private Transform startTransform;
+//    private bool isUpdatingXPosition;
+//    private bool isCornerTurnerVirgin;
+//    CheckPointManager checkPointManager;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        startTransform = GameObject.FindGameObjectWithTag("StartTransform").transform;
-        isUpdatingXPosition = true;
-        isCornerTurnerVirgin = true;
-        checkPointManager = GameObject.FindGameObjectWithTag("CheckPointManager").GetComponent<CheckPointManager>();
-    }
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        startTransform = GameObject.FindGameObjectWithTag("StartTransform").transform;
+//        isUpdatingXPosition = true;
+//        isCornerTurnerVirgin = true;
+//        checkPointManager = GameObject.FindGameObjectWithTag("CheckPointManager").GetComponent<CheckPointManager>();
+//    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        GetHorizontalPosition();
-        SetHorizontalPosition();
-    }
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        GetHorizontalPosition();
+//        SetHorizontalPosition();
+//    }
 
-    void GetHorizontalPosition()
-    {
+//    void GetHorizontalPosition()
+//    {
 
-        if (!isCornerTurnerVirgin)
-        {
-            if (isUpdatingXPosition)
-            {
-                position = new Vector3(checkPointManager.lastCornerTurned.transform.position.x, transform.position.y, transform.position.z);
-                isUpdatingXPosition = false;
-            }
-            else
-            {
-                position = new Vector3(transform.position.x, transform.position.y, checkPointManager.lastCornerTurned.transform.position.z);
-                isUpdatingXPosition = true;
+//        if (!isCornerTurnerVirgin)
+//        {
+//            if (isUpdatingXPosition)
+//            {
+//                position = new Vector3(checkPointManager.lastCornerTurned.transform.position.x, transform.position.y, transform.position.z);
+//                isUpdatingXPosition = false;
+//            }
+//            else
+//            {
+//                position = new Vector3(transform.position.x, transform.position.y, checkPointManager.lastCornerTurned.transform.position.z);
+//                isUpdatingXPosition = true;
 
-            }
-        }
-        else
-        {
-            position = new Vector3(startTransform.position.x, transform.position.y, transform.position.z);
-            isCornerTurnerVirgin = false;
-        }
-    }
+//            }
+//        }
+//        else
+//        {
+//            position = new Vector3(startTransform.position.x, transform.position.y, transform.position.z);
+//            isCornerTurnerVirgin = false;
+//        }
+//    }
 
-    void SetHorizontalPosition()
-    {
-        transform.position = position;
-    }
+//    void SetHorizontalPosition()
+//    {
+//        transform.position = position;
+//    }
 
 
-}
+//}
