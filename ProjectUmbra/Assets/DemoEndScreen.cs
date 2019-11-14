a9bd0b91a1d5c26c0b5cb6bb489f04a357c38009
disaviola <37692445+disaviola@users.noreply.github.com>
2019-11-14 16:42:35 +0100

    Endscene

---
+++ ProjectUmbra/Assets/DemoEndScreen.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DemoEndScreen : MonoBehaviour
+{
+    [SerializeField] int sceneToLoad = 0;
+    void LoadMainMenu()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            LoadMainMenu();
+        }
+    }
+}
