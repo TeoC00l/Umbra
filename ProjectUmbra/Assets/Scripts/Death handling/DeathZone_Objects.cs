2d86acb31eeef966a508f65017b5a8986f693143
disaviola <37692445+disaviola@users.noreply.github.com>
2019-11-21 10:26:00 +0100

    Death component for dangerous objects

---
+++ ProjectUmbra/Assets/Scripts/Death handling/DeathZone_Objects.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZone_Objects : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            DeathComponent dc = ObjectHandeler.Player.GetComponent<DeathComponent>();
+            dc.RespawnPlayer();
+        }
+    }
+}
