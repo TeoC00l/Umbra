0f252e7e27606e52e9e09ebc65ec9131927749f5
disaviola <37692445+disaviola@users.noreply.github.com>
2019-11-28 16:30:51 +0100

    sorting & music, audio player

---
+++ ProjectUmbra/Assets/Scripts/BoxSpawn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxSpawn : MonoBehaviour
+{
+
+    private Vector3 spawnLocation;
+
+
+    private void Awake()
+    {
+        spawnLocation = transform.position;
+    }
+
+
+    public void RespawnBox()
+    {
+        transform.position = spawnLocation + new Vector3(0, 1, 0);
+    }
+}
---
+++ ProjectUmbra/Assets/Scripts/KarmaDependentDialogueTrigger.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KarmaDependentDialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue1, dialogue2;
+    [SerializeField] private DialogueManager dm;
+    [Header("Should player input be disabled during dialogue?")]
+    [SerializeField] bool holdPlayer = true;
+    [Header("Should dialogue disappear by itself after five seconds?")]
+    [SerializeField] bool hasTimer = false;
+    [SerializeField] private int karmaForOption1, karmaForOption2;
+
+
+    //the object this script is attached to will be the trigger for the dialogue. It could be a collider trigger, button, etc. or be called from another script for scripted cutscene events.
+
+    public void Start()
+    {
+        //dm = FindObjectOfType<DialogueManager>();
+    }
+
+    //call this function by any method to start the dialogue, for example ontriggerenter
+    public void TriggerDialogue()
+    {
+        dm.dialogueCanvas.SetActive(true);
+
+        if (hasTimer)
+        {
+            if(KarmaManager.Karma == karmaForOption1)
+            {
+                dm.StartDialogueWithTimer(dialogue1, holdPlayer);
+            } else if(KarmaManager.Karma == karmaForOption2)
+            {
+                dm.StartDialogueWithTimer(dialogue2, holdPlayer);
+            }
+            
+        }
+        else
+        {
+            if (KarmaManager.Karma == karmaForOption1)
+            {
+                dm.StartDialogue(dialogue1, holdPlayer);
+            }
+            else if (KarmaManager.Karma == karmaForOption2)
+            {
+                dm.StartDialogue(dialogue2, holdPlayer);
+            }
+
+        }
+
+    }
+}
