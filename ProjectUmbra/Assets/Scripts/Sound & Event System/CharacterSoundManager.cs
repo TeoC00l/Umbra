30009b80879cc95b7c90004db760e4b25a05621c
disaviola <37692445+disaviola@users.noreply.github.com>
2019-11-29 10:04:21 +0100

    Oz Footsteps for WB2

---
+++ ProjectUmbra/Assets/Scripts/Sound & Event System/CharacterSoundManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSoundManager : MonoBehaviour
+{
+    private AudioSource source;
+    [SerializeField] private AudioClip footSteps;
+
+    private void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    public void PlayFootSteps()
+    {
+        source.PlayOneShot(footSteps);
+    }
+}
