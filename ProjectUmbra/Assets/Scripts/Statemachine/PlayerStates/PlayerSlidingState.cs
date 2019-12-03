3e88e8f9e3558d38fd52fe1b16538504047ced58
TeoC00l <37851342+TeoC00l@users.noreply.github.com>
2019-12-03 16:45:09 +0100

    Update PlayerSlidingState.cs

--- ProjectUmbra/Assets/Scripts/Statemachine/PlayerStates/PlayerSlidingState.cs
+++ ProjectUmbra/Assets/Scripts/Statemachine/PlayerStates/PlayerSlidingState.cs
@@ -6,24 +6,40 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Player/PlayerSlideState")]
 public class PlayerSlidingState : BaseState
 {
-
-
+    private int framesPassed;
+    private LayerMask layerMask;
 
     public override void Enter()
     {
+        layerMask = new LayerMask();
+        layerMask = ~0;
         Debug.Log("Player enter slide state");
-
+        animator.SetBool("isFalling", false);
+        animator.SetBool("isSliding", true);
+        framesPassed = 0;
 
     }
 
     public override void HandleFixedUpdate()
     {
-        if (playerMovement.IsGrounded())
+        framesPassed++;
+        if (playerMovement.IsGrounded() && framesPassed > 10)
         {
 
             owner.Transition<WalkState>();
         }
+
+            if (!Physics.Raycast(owner.transform.position, Vector3.down, 5f, layerMask, QueryTriggerInteraction.Ignore))
+            {
+            animator.SetBool("isSliding", false);
+            animator.SetBool("isFalling", true);
+        }
+
     }
 
-
+    public override void Exit()
+    {
+        animator.SetBool("isFalling", false);
+        animator.SetBool("isSliding", false);
+    }
 }
