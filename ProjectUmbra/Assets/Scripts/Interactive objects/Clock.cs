284193870c80f38389725d4854be8c79aa4418c7
TeoC00l <37851342+TeoC00l@users.noreply.github.com>
2019-12-05 18:36:28 +0100

    clock in shop with hard R

---
+++ ProjectUmbra/Assets/Scripts/Interactive objects/Clock.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+    [SerializeField] private Transform hourTransform, minuteTransform, secondTransform;
+    [SerializeField] private float degreesPerHour;
+    [SerializeField] private float degreesPerMinute;
+    [SerializeField] private float degreesPerSecond;
+    [SerializeField] private bool continous;
+
+    void Update()
+    {
+        if (continous)
+        {
+            UpdateContinously();
+        }
+        else
+        {
+            UpdateDiscrete();
+        }
+    }
+
+    private void UpdateContinously()
+    {
+        TimeSpan time = DateTime.Now.TimeOfDay;
+        hourTransform.localRotation = Quaternion.Euler(0f, (float) time.TotalHours * degreesPerHour, 0f);
+        minuteTransform.localRotation = Quaternion.Euler(0f, (float) time.TotalMinutes * degreesPerMinute, 0f);
+        secondTransform.localRotation = Quaternion.Euler(0f, (float) time.TotalSeconds * degreesPerSecond, 0f);
+    }
+
+    private void UpdateDiscrete()
+    {
+        DateTime time = DateTime.Now;
+        hourTransform.localRotation = Quaternion.Euler(0f, time.Hour * degreesPerHour, 0f);
+        minuteTransform.localRotation = Quaternion.Euler(0f, DateTime.Now.Minute * degreesPerMinute, 0f);
+        secondTransform.localRotation = Quaternion.Euler(0f, DateTime.Now.Second * degreesPerSecond, 0f);
+    }
+}
