41cf9f9cd131b961c85ed27aed166390ff901e1c
TeoC00l <37851342+TeoC00l@users.noreply.github.com>
2019-12-09 11:37:47 +0100

    Cutscenes & prologue-churchtohospital

---
+++ ProjectUmbra/Assets/MeshSceneRotator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshSceneRotator : MonoBehaviour
+{
+    public int cornerTurnerMode;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player.GetComponent<PlayerMovement>().cornerTurnerMode = this.cornerTurnerMode;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void RotateOnInitialize()
+    {
+        
+    }
+}
