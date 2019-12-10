e27cc75e499a4578ceba74e6f614df9bdecc40e0
Disa Sahlström <disa7213@win.dsv.su.se>
2019-12-10 17:47:24 +0100

    Cutscenestuff

---
+++ ProjectUmbra/Assets/Church3_CutsceneController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Church3_CutsceneController : MonoBehaviour
+{
+    public DialogueManager isSetDone;
+    public GameObject player;
+    public ParticleSystem particles;
+
+
+    private void Start()
+    {
+
+    }
+    private void Update()
+    {
+        if (isSetDone.Done)
+        {
+            StartCoroutine(DisappearOz());
+        }
+    }
+
+    private IEnumerator DisappearOz()
+    {
+        particles.transform.position = player.transform.position;
+        particles.Play();
+        yield return new WaitForSeconds(2);
+        player.SetActive(false);
+        ObjectHandeler.DeathScreen.GetComponent<DeathscreenTimer>().FadeAndChangeScene("Cutscene_BadEnding3");
+    }
+}
