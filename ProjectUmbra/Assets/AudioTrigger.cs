698898fe315014736fda32b3ef581ebb7360d77b
Disa Sahlström <disa7213@win.dsv.su.se>
2019-12-16 17:36:27 +0100

    Bell sound for store, audiotrigger prefab

---
+++ ProjectUmbra/Assets/AudioTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTrigger : MonoBehaviour
+{
+
+    public string soundToPlay;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            AudioManager.instance.Play(soundToPlay);
+            gameObject.SetActive(false);
+        }
+    }
+}
