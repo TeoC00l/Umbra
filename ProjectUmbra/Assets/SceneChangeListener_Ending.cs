03479d9343692ad755b6a9374b1c9ae028f5e73a
Disa Sahlström <disa7213@win.dsv.su.se>
2020-01-06 17:42:12 +0100

    Good ending updated

---
+++ ProjectUmbra/Assets/SceneChangeListener_Ending.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class SceneChangeListener_Ending : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    public string sceneToLoad = "";
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!videoPlayer.isPlaying)
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+}
