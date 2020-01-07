1a4a212b6406b0c6103be546e7c3bfab725147c6
Disa Sahlström <disa7213@win.dsv.su.se>
2020-01-07 15:11:13 +0100

    Sweeeet shop transition

---
+++ ProjectUmbra/Assets/MysteryShop_Timeline.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Aura2API;
+
+public class MysteryShop_Timeline : MonoBehaviour
+{
+
+    public ParticleSystem shopkeeperParticles, playerParticles;
+    public Light[] lights;
+    public AuraVolume mist;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TriggerSKParticles()
+    {
+        shopkeeperParticles.Play();
+    }
+
+    public void TriggerPlayerParticles()
+    {
+        playerParticles.Play();
+    }
+
+    public void ChangeLights()
+    {
+        foreach(Light l in lights)
+        {
+            l.color = Color.magenta;
+        }
+        mist.lightInjection.color = Color.magenta;
+    }
+}
