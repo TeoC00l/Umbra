aad188f39ca8918ad34495d82e00e6ccdbf1d496
Bazzan <sebake01@gmail.com>
2020-02-18 16:42:12 +0100

    Scean changer + mpore speed in animation

--- ProjectUmbra/Assets/Scripts/Managers and controllers/Scenechanging/SceneChangeTrigger_ByString.cs
+++ ProjectUmbra/Assets/Scripts/Managers and controllers/Scenechanging/SceneChangeTrigger_ByString.cs
@@ -14,18 +14,40 @@ public class SceneChangeTrigger_ByString : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             ObjectHandeler.Player.GetComponent<PlayerMovement>().setSpeed(0);
+
             StartCoroutine(FadeOut());
         }
     }
 
     public IEnumerator FadeOut()
     {
+
         fadeScreen.canvasRenderer.SetAlpha(0.0f);
         fadeScreen.CrossFadeAlpha(1, 0.5f, false);
         yield return new WaitForSeconds(2);
-        SceneManager.LoadScene(sceneToChangeTo);
+        StartCoroutine(LoadYourAsyncScene());
+
+        //SceneManager.LoadScene(sceneToChangeTo);
+
     }
 
+
+    IEnumerator LoadYourAsyncScene()
+    {
+        // The Application loads the Scene in the background as the current Scene runs.
+        // This is particularly good for creating loading screens.
+        // You could also load the Scene by using sceneBuildIndex. In this case Scene2 has
+        // a sceneBuildIndex of 1 as shown in Build Settings.
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneToChangeTo);
+        
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+    }
 }
 
 
